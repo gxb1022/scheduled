3eb6266a4e4ea63a6351513222cba93b600bd0b8
gxb1022 <guoxiaobinnihao@sina.com>
2020-08-27 09:52:56 +0800

    学习回滚

--- src/main/java/com/example/scheduled/Test.java
+++ src/main/java/com/example/scheduled/Test.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 public class Test {
 
     public static void main(String[] args) {
-//        Person person = new Person.Builder().age(23).name("ssss").build();
-//        System.out.println(person.toString());
+        Person person = new Person.Builder().age(23).name("ssss").build();
+        System.out.println(person.toString());
         File file = new File("C:/Users/86159/Desktop/郭晓斌.docx");
         try(FileInputStream fis = new FileInputStream(file)){
             //代码逻辑
